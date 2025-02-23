1476e3cc9b8058de0ac5888b3daf590676e39ec8
Polqt <poyhidalgo@gmail.com>
2025-02-23 15:41:18 +0800

    chore: add dark mode support with next-themes; update Header and Home components; enhance layout with consistent casing in file names

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
 import Header from "@/components/Header";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="w-full max-w-3xl mx-auto pt-10 md:pt-16 px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center gap-x-3">
+          <Avatar>
+            <AvatarImage 
+              src="https://scontent-mnl1-2.xx.fbcdn.net/v/t39.30808-6/478574169_9077837935604452_3537622823525496935_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHH5fRapQnAOHZWoQlq-5QJJy11jp23DSgnLXWOnbcNKHsgefMgUcRRxVPe5kWVq8phu1Dicm90KWfkbQEI43MU&_nc_ohc=EfvRQCs3HLAQ7kNvgEZWAnf&_nc_oc=AdjEovaIU6DK7yK3s1397dMONmMiU-5_nGAU3CqiXYSIJ3rxHkePrXBo-xEfa6cu9q8&_nc_zt=23&_nc_ht=scontent-mnl1-2.xx&_nc_gid=ACnvfGZGoxns7Cn5AbhmxEW&oh=00_AYA3j-hTbXAM7sxqYkyBOH6lwU17ikjZxurCkSt8UZFi3Q&oe=67C06536" 
+              width={1000}
+            />
+          </Avatar>
+          <div className="grow">
+            <h2 className="text-2xl font-bold sm:text-4xl">Hi, I&apos;m Pol Hidalgo </h2>
+            <h5 className="mt-1 text-base font-semibold text-gray-900">Student Software Engineer</h5>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,30 @@
 'use client'
 
-import { Home, Mail, Moon, User } from "lucide-react";
+import { Home, Mail, Moon, Sun, User } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 
 export default function Header() {
     // TODO: Implement Dark Mode
+    const {theme, setTheme} = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
 
     return(
-        <header className="sticky top-0 inset-x-0 flex lfex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
-            <nav className="mt-5 relative max-w-2xl w-full bg-white border border-gray-200 rounded-[2rem] py-1 flex items-center justify-between md:px-4 md:mx-auto opacity-90">
+        <header className="sticky top-0 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
+            <nav className="mt-5 relative max-w-3xl w-full bg-white border border-gray-200 rounded-[2rem] py-1 flex items-center justify-between md:px-4 md:mx-auto opacity-90">
                 <Avatar className="w-8 h-8">
                     <AvatarImage 
-                        src="https://scontent-mnl1-2.xx.fbcdn.net/v/t39.30808-6/478574169_9077837935604452_3537622823525496935_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHH5fRapQnAOHZWoQlq-5QJJy11jp23DSgnLXWOnbcNKHsgefMgUcRRxVPe5kWVq8phu1Dicm90KWfkbQEI43MU&_nc_ohc=EfvRQCs3HLAQ7kNvgEZWAnf&_nc_oc=AdjEovaIU6DK7yK3s1397dMONmMiU-5_nGAU3CqiXYSIJ3rxHkePrXBo-xEfa6cu9q8&_nc_zt=23&_nc_ht=scontent-mnl1-2.xx&_nc_gid=ACnvfGZGoxns7Cn5AbhmxEW&oh=00_AYA3j-hTbXAM7sxqYkyBOH6lwU17ikjZxurCkSt8UZFi3Q&oe=67C06536" 
+                        src="/avatar.png" 
                     />
-                    <AvatarFallback>Jepoy</AvatarFallback>
+                    <AvatarFallback>JP</AvatarFallback>
                 </Avatar>
 
 
@@ -36,8 +44,12 @@ export default function Header() {
                             <Mail />
                         </Button>
                     </Link>
-                    <Button variant={"ghost"} size={"icon"}>
-                        <Moon />
+                    <Button 
+                        variant={"ghost"} 
+                        size={"icon"}
+                        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+                    >
+                        {mounted ? (theme === 'dark' ? <Sun /> : <Moon />) : <Moon />}
                     </Button>
                 </div>
             </nav>
