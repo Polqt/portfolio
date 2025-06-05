759654627e1ff5649a5273a9a86beaf6811f98d8
Jepoy <poyhidalgo@gmail.com>
2025-06-05 16:48:10 +0800

    refactor: replace Header component with Dock in ProjectsContent for consistent layout; remove unused direction-aware-hover component

--- components/ProjectsContent.tsx
+++ components/ProjectsContent.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import Header from '@/components/Header';
+import Dock from '@/components/Dock';
 
 export default function ProjectsContent() {
   return (
     <div>
-      <Header />
+      <Dock />
 
       <div className="w-full max-w-3xl mx-auto pt-10 md:pt-16 px-4 sm:px-6 lg:px-8"></div>
     </div>
